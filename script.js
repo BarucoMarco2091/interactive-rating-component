7fa45be0f629918fa97b0c5b23129f274ad40658
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-08-19 16:58:25 -0300

    end

---
+++ script.js
@@ -0,0 +1,16 @@
+let btnSubmit = document.getElementById('submit');
+btnSubmit.addEventListener('click', () => {
+    let r = document.getElementById('res')
+    let radio = document.querySelector('input[name="r1"]:checked').value;
+    r.innerText = radio;
+    const container = document.querySelector('#container');
+    container.outerHTML = `
+<div id="containernew">
+    <img src="assets/illustration-thank-you.png" width="20%" height="20%">
+    <span>You selected ${radio} out of 5
+    <h1 id="new-title">Thank you!</h1>
+    <p>We appreciate you taking the time to give a rating.<br>If you ever need more support, don't hesitate to<br>get in touch!</p>
+</div>
+`
+});
+
